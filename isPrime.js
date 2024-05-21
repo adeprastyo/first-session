d39f74fc50e7c52b72ec4eaec29fadcc62a5e7d9
adeprastyo <adeprastyo101@gmail.com>
2024-05-22 01:35:54 +0700

    tugas session 1

--- isPrime.js
+++ isPrime.js
@@ -5,6 +5,11 @@ Jika bilangan prima maka return true, jika bukan return false.
 
 function isPrime(num) {
   // Code here
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 console.log(isPrime(7)); // Output: true
